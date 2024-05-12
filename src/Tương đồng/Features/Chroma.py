1b9426141dfc41965eabbcc4654d86af905a058b
Believemyself16 <han8b8@gmail.com>
2024-05-12 18:25:55 +0700

    commit trích rút 3 đặc trưng chroma, mfcc, filterbank

--- src/Tương đồng/Features/Chroma.py
+++ src/Tương đồng/Features/Chroma.py
@@ -1,17 +1,10 @@
-import numpy as np
 import librosa
 
-def extract_chroma(audio_path, sr=22050):
-    """
-    Trích xuất đặc trưng Chroma từ tệp âm thanh.
-
-    Parameters:
-        audio_path (str): Đường dẫn đến tệp âm thanh.
-        sr (int): Tần số lấy mẫu.
-
-    Returns:
-        np.ndarray: Ma trận chứa các hệ số Chroma.
-    """
-    y, sr = librosa.load(audio_path, sr=sr)
-    chroma = librosa.feature.chroma_stft(y=y, sr=sr)
-    return chroma.T
+def extract_chroma(audio_path):
+    # Đọc file âm thanh và tính chroma
+    y, sr = librosa.load(audio_path)
+    
+    #trích xuất đặc trưng chroma
+    chroma = librosa.feature.chroma_stft(y=y, sr=sr) #y là mảng chứa dữ liệu âm thanh, sr là tần số lấy mẫu
+    
+    return chroma
